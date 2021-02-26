e5f56ba9dba312847478d9381feaea32236179bc
Anders Hald <chald80@gmail.com>
2021-02-26 11:18:34 +0100

    update

--- DBClient.cs
+++ DBClient.cs
@@ -10,6 +10,8 @@ namespace ScandicHotelDB
     class DBClient
     {
 
+
+
         string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScandicHotelDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
         public void DbQuery(string QueryString)
@@ -59,6 +61,26 @@ namespace ScandicHotelDB
                 Console.Write("{0,-18}", record[i]);
             }
             Console.WriteLine();
+
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Hotel DataBase");
+            DbReader("Select * From Hotel");
+            Console.WriteLine("Booking DataBase");
+            DbReader("Select * From Booking");
+            Console.WriteLine("Room DataBase");
+            DbReader("select * from Room");
+            Console.WriteLine("Guest DataBase");
+            DbReader("select * from Guest");
+            Console.WriteLine("Hotel DataBase after insert");
+            DbQuery("INSERT INTO Hotel VALUES (101,'The Pope','Vaticanstreet 1 1111 Bishopcity')");
+            DbReader("Select * From Hotel");
+            Console.WriteLine("Hotel DataBase after delete");
+            DbQuery("Delete from Hotel where Hotel_No = 101 ");
+            DbReader("Select * From Hotel");
+            Console.WriteLine("Hotel DataBase done");
 
         }
 
--- Program.cs
+++ Program.cs
@@ -8,28 +8,8 @@ namespace ScandicHotelDB
         static void Main(string[] args)
         {
             DBClient DB = new DBClient();
-            
+            DB.Start();
 
-            Console.WriteLine("Hotel DataBase");
-            DB.DbReader("Select * From Hotel");
-            Console.WriteLine("Booking DataBase");
-            DB.DbReader("Select * From Booking");
-            Console.WriteLine("Room DataBase");
-            DB.DbReader("select * from Room");
-            Console.WriteLine("Guest DataBase");
-            DB.DbReader("select * from Guest");
-            Console.WriteLine("Hotel DataBase after insert");
-            DB.DbQuery("INSERT INTO Hotel VALUES (101,'The Pope','Vaticanstreet 1 1111 Bishopcity')");
-            DB.DbReader("Select * From Hotel");
-            Console.WriteLine("Hotel DataBase after delete");
-            DB.DbQuery("Delete from Hotel where Hotel_No = 101 ");
-            DB.DbReader("Select * From Hotel");
-            Console.WriteLine("Hotel DataBase done");
-
-
-
-
-            Console.WriteLine();
 
         }
     }
