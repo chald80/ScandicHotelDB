d0e607015025a315cf3dde6f07797858bad3f6cc
Anders Hald <chald80@gmail.com>
2021-03-02 09:58:20 +0100

    update Start

--- DBClient.cs
+++ DBClient.cs
@@ -53,14 +53,45 @@ namespace ScandicHotelDB
 
         static void ReadSingleRow(IDataRecord record)
         {
-            
+
             for (int i = 0; i < record.FieldCount; i++)
             {
                 Console.Write("{0,-18}", record[i]);
             }
+
             Console.WriteLine();
+        }
+
+        public void Start()
+        {
+            
+            
+            Console.WriteLine("Hotel DataBase");
+            DbReader("Select * From Hotel");
+            Console.WriteLine("Booking DataBase");
+            DbReader("Select * From Booking");
+            Console.WriteLine("Room DataBase");
+            DbReader("select * from Room");
+            Console.WriteLine("Guest DataBase");
+            DbReader("select * from Guest");
+            Console.WriteLine("Hotel DataBase after insert");
+            DbQuery("INSERT INTO Hotel VALUES (101,'The Pope','Vaticanstreet 1 1111 Bishopcity')");
+            DbReader("Select * From Hotel");
+            Console.WriteLine("Hotel DataBase after delete");
+            DbQuery("Delete from Hotel where Hotel_No = 101 ");
+            DbReader("Select * From Hotel");
+            Console.WriteLine("Hotel DataBase done");
+
+
+
+
 
         }
+
+
+
+
+
 
     }
 
--- Program.cs
+++ Program.cs
@@ -9,27 +9,12 @@ namespace ScandicHotelDB
         {
             DBClient DB = new DBClient();
             
-
-            Console.WriteLine("Hotel DataBase");
-            DB.DbReader("Select * From Hotel");
-            Console.WriteLine("Booking DataBase");
-            DB.DbReader("Select * From Booking");
-            Console.WriteLine("Room DataBase");
-            DB.DbReader("select * from Room");
-            Console.WriteLine("Guest DataBase");
-            DB.DbReader("select * from Guest");
-            Console.WriteLine("Hotel DataBase after insert");
-            DB.DbQuery("INSERT INTO Hotel VALUES (101,'The Pope','Vaticanstreet 1 1111 Bishopcity')");
-            DB.DbReader("Select * From Hotel");
-            Console.WriteLine("Hotel DataBase after delete");
-            DB.DbQuery("Delete from Hotel where Hotel_No = 101 ");
-            DB.DbReader("Select * From Hotel");
-            Console.WriteLine("Hotel DataBase done");
+            DB.Start();
+            
 
 
 
-
-            Console.WriteLine();
+            
 
         }
     }
