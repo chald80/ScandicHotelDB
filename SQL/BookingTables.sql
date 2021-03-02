09adf471ca66d49fa47d16a066bb64bd6aad425a
Anders Hald <chald80@gmail.com>
2021-03-02 12:12:17 +0100

    update

---
+++ SQL/BookingTables.sql
@@ -0,0 +1,1 @@
+CREATE TABLE Booking( Booking_id int IDENTITY(1,1) NOT NULL PRIMARY KEY, Hotel_No int NOT NULL, Guest_No int NOT NULL, Date_From DATE NOT NULL, Date_To DATE NOT NULL, Room_No int NOT NULL, FOREIGN KEY(Guest_No) REFERENCES Guest(Guest_No), FOREIGN KEY(Room_No, Hotel_No) REFERENCES Room(Room_No, Hotel_No) );
